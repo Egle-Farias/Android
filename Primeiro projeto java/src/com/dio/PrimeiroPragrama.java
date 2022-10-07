85550bc3cd923b4930e7c5bce44b064313b1e14c
Egle-Farias <suporto.eglefarias@gmail.com>
2022-10-07 00:08:52 -0300

    delete media

---
+++ Primeiro projeto java/src/com/dio/PrimeiroPragrama.java
@@ -0,0 +1,28 @@
+package com.dio;
+
+import com.dio.modeo.Gato;
+
+public class PrimeiroPragrama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2;
+		int b = 3; 
+		
+              System.out.println("Helloword " + (a + b));
+     System.out.println(b -a);
+              
+              
+              Gato gato = new Gato();     
+                  
+          
+    	System.out.println(gato);
+         
+          
+              
+           
+            
+            
+	}
+
+}
